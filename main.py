da48274813ccbf5581e0a36ed82c0e13ab2633c2
Paige Adams <padams87@icloud.com>
2020-02-28 16:59:06 -0500

    Working on it

--- main.py
+++ main.py
@@ -6,32 +6,50 @@ __author__ = "???"
 
 def add(x, y):
     """Add two integers. Handles negative values."""
-    # your code here
-    return
+    sum = x + y
+    return sum
 
 
 def multiply(x, y):
     """Multiply x with y. Handles negative values of x or y."""
-    # your code here
-    return
+    product = 0
+    if y >= 0:
+        for i in range(y):
+            product = add(product, x)
+    else:
+        for j in range(-y):
+            product -= x
+    return product
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0"""
-    # your code here
-    return
+    result = x
+
+    if n >= 0:
+        nums = range(1, n)
+        for i in nums:
+            result = multiply(result, x)
+    return result
+
+print(power(-2, 4))
+
 
 
 def factorial(x):
     """Compute factorial of x, where x > 0"""
-    # your code here
-    return
+    result = 1
+    for i in range(1, x + 1):
+        result = multiply(result, i)
+    return result
 
 
 def fibonacci(n):
     """Compute the nth term of fibonacci sequence"""
-    # your code here
-    return
+    num_list = [0, 1, 1]
+    for i in range(2, n):
+        num_list.append(num_list[-1] + num_list[-2])
+    return num_list[n-1]
 
 
 if __name__ == '__main__':
